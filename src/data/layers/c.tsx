50797d45991ae0ff4b20aee894df60dbc75b19d8
H4zardZ1 <101616102+H4zardZ1@users.noreply.github.com>
2023-08-14 20:24:34 +0700

    new layer (unfinished)

---
+++ src/data/layers/c.tsx
@@ -0,0 +1,25 @@
+/**
+ * @module
+ * @hidden
+ */
+import { main } from "data/projEntry";
+import { createCumulativeConversion } from "features/conversion";
+import { jsx } from "features/feature";
+import { createHotkey } from "features/hotkey";
+import { createReset } from "features/reset";
+import MainDisplay from "features/resources/MainDisplay.vue";
+import { createResource } from "features/resources/resource";
+import { addTooltip } from "features/tooltips/tooltip";
+import { createResourceTooltip } from "features/trees/tree";
+import { BaseLayer, createLayer } from "game/layers";
+import type { DecimalSource } from "util/bignum";
+import { render } from "util/vue";
+import { createLayerTreeNode, createResetButton } from "../common";
+
+const id = "c";
+const layer = createLayer(id, function (this: BaseLayer) {
+    const name = "Core"
+    return {
+        display: jsx(() => <>My layer</>)
+    }
+})
