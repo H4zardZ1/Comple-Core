f7ee4665c6c8e9a18d4764091bda92dc894302be
H4zardZ1 <101616102+H4zardZ1@users.noreply.github.com>
2023-08-15 14:22:26 +0700

    what did i do again

--- src/data/layers/c.tsx
+++ src/data/layers/c.tsx
@@ -5,6 +5,8 @@
 import { main } from "data/projEntry";
 import { createCumulativeConversion } from "features/conversion";
 import { jsx } from "features/feature";
+import { createUpgrade } from "features/upgrades/upgrade"
+import { createCostRequirement } from "game/requirements"
 import { createHotkey } from "features/hotkey";
 import { createReset } from "features/reset";
 import MainDisplay from "features/resources/MainDisplay.vue";
@@ -15,11 +17,32 @@ import { BaseLayer, createLayer } from "game/layers";
 import type { DecimalSource } from "util/bignum";
 import { render } from "util/vue";
 import { createLayerTreeNode, createResetButton } from "../common";
+import { noPersist } from "game/persistence";
 
 const id = "c";
 const layer = createLayer(id, function (this: BaseLayer) {
     const name = "Core"
+    const unlockMainStuff = createUpgrade(() => ({
+        requirements: createCostRequirement(() => ({
+            resource: noPersist(main.points),
+            cost: 1
+    })),
+        display: {
+            description: "Start doing stuff."
+        }
+    }))
+
+
+
+    
     return {
-        display: jsx(() => <>My layer</>)
+        name,
+        unlockMainStuff,
+        display: jsx(() => (
+            <>
+                <MainDisplay resource={main.points} />
+                {render(unlockMainStuff)}
+            </>
+        ))
     }
 })
